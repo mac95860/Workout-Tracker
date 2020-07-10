00f85d761c47453d82594299201dfe72bd81fdc7
Michael Cervantes <mac95860@hotmail.com>
2020-07-10 17:29:35 -0500

    At least TWO api routes work

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,45 +1,55 @@
 const Router = require("express").Router;
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const mongo = require("mongojs");
 
 const db = require("../models");
 
 const apiRoutes = new Router();
 
 
-//create new workout
+//create new workout !!!!!!!WORKS!!!!!!!!!!!!!!
 apiRoutes.post("/workouts", async (req, res) => {
     const data = await db.Workout.create({type: "workout"});
     res.json(data);
 })
 
 //create new exercise
-apiRoutes.post("/exercise", ({body}, res) => {
-    db.Workout.create(body)
-    .then((body) => db.Workout.findOneAndUpdate({}, {$push: {exercise: body}}, {new: true} ))
-    .then(dbWorkout => {
-        res.json(dbWorkout);
-    }). catch (err => {
-        res.json(err);
-    })
+// apiRoutes.put("/exercise", ({body}, res) => {
+//     db.Workout.create(body)
+//     .then((body) => db.Workout.findOneAndUpdate({_id: mongojs.ObjectId(params)}, {$push: {exercise: body}}, {new: true} ))
+//     .then(dbWorkout => {
+//         res.json(dbWorkout);
+//     }). catch (err => {
+//         res.json(err);
+//     })
         
-})
+// })
 
-//get workouts
-apiRoutes.get("/api/workouts", (req, res) => {
+//get workouts !!!!!!!!!!!!WORKS!!!!!!!!!!!!!!!!!!
+apiRoutes.get("/workouts", (req, res) => {
     db.Workout.find({})
         .then((data) => {
             res.json(data);
         })
 });
 
-//update workout
+//update workout (this is the api the api.js file in public calls)
 apiRoutes.put("/workouts/:id", ({body, params}, res) => {
-    db.Workout.find({_id: req.params.id})
+    db.Workout.findOneAndUpdate({_id: mongojs.ObjectId(params)}, {$push: {exercise: body}})
         .then(dbWorkout => {
-
+            res.json(dbWorkout);
+            if (err) {
+                console.log(err);
+            }
         })
 
+});
+
+apiRoutes.get("/workouts/range", (req, res) => {
+    db.Workout.find({}, (err, data) => {
+        res.json(data);
+    })
 })
 
 module.exports = apiRoutes;
