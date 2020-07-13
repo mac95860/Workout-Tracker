5c7d22932a08c3fa9fb2829acb5e51df8d99c19e
Michael Cervantes <mac95860@hotmail.com>
2020-07-13 12:47:32 -0500

    Almost there

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -14,18 +14,6 @@ apiRoutes.post("/workouts", async (req, res) => {
     res.json(data);
 })
 
-//create new exercise
-// apiRoutes.put("/exercise", ({body}, res) => {
-//     db.Workout.create(body)
-//     .then((body) => db.Workout.findOneAndUpdate({_id: mongojs.ObjectId(params)}, {$push: {exercise: body}}, {new: true} ))
-//     .then(dbWorkout => {
-//         res.json(dbWorkout);
-//     }). catch (err => {
-//         res.json(err);
-//     })
-        
-// })
-
 //get workouts !!!!!!!!!!!!WORKS!!!!!!!!!!!!!!!!!!
 apiRoutes.get("/workouts", (req, res) => {
     db.Workout.find({})
@@ -36,16 +24,15 @@ apiRoutes.get("/workouts", (req, res) => {
 
 //update workout (this is the api the api.js file in public calls)
 apiRoutes.put("/workouts/:id", ({body, params}, res) => {
-    db.Workout.findOneAndUpdate({_id: mongojs.ObjectId(params)}, {$push: {exercise: body}})
+    db.Workout.findOneAndUpdate({_id: mongo.ObjectId(params.id)}, {$push: {exercise: body}})
         .then(dbWorkout => {
             res.json(dbWorkout);
-            if (err) {
-                console.log(err);
-            }
+            console.log(dbWorkout);
         })
 
 });
 
+//!!!!!!!!!!!!!!!!!works!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 apiRoutes.get("/workouts/range", (req, res) => {
     db.Workout.find({}, (err, data) => {
         res.json(data);
